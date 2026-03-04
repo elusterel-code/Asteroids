02060ce1c511234327d2be35de56c6729e776d71
Eddie Luster <eluster.el@gmail.com>
2026-03-04 15:13:32 -0600

    G: added move

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_SPEED
 from constants import PLAYER_TURN_SPEED
 
 class Player(CircleShape):
@@ -27,6 +27,17 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotation += PLAYER_TURN_SPEED * dt    
-        if keys[pygame.K_d]:
             self.rotation -= PLAYER_TURN_SPEED * dt    
+        if keys[pygame.K_d]:
+            self.rotation += PLAYER_TURN_SPEED * dt
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+        
